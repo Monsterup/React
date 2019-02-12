2aad4bbe6f1751b031a26801af83843a715fc157
Monsterup <yudhamanage@gmail.com>
2019-02-12 13:31:27 +0700

    Pertemuan Kedua

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Product from './Product';
 import * as serviceWorker from './serviceWorker';
 
 // const element = <h1>Hello world on variable</h1>
@@ -26,18 +27,57 @@ import * as serviceWorker from './serviceWorker';
 
 // const element = <h1>Halo, {getMahasiswa(mahasiswi)}, anda {getStatus(mahasiswi)}</h1>
 
-function tick(){
-    const element = (
-        <div>
-            <h1>Hello Cuyyy</h1>
-            <h2>It is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+// function tick(){
+//     const element = (
+//         <div>
+//             <h1>Hello Cuyyy</h1>
+//             <h2>It is {new Date().toLocaleTimeString()}</h2>
+//         </div>
+//     );
 
-    ReactDOM.render(element, document.getElementById('root'));
-}
+//     ReactDOM.render(element, document.getElementById('root'));
+// }
 
-setInterval(tick,1000);
+// setInterval(tick,1000);
+
+/* ========== Component & Props ========== */
+
+// function Product(props){
+//     return (
+//         <div>
+//             <h1>{props.judul}</h1>
+//             <h3>Harga : {props.harga}</h3>
+//         </div>
+//     );
+// }
+
+/* VERSI SENDIRI
+const element = {
+    judul : 'Sempak Turbo',
+    harga : "1M"
+};
+
+ReactDOM.render(Product(element), document.getElementById('root'));
+*/
+
+// const element = <Product judul = "Sempak Turbo" harga = "1M"></Product>;
+
+// ReactDOM.render(element, document.getElementById('root'));
+
+// class Product extends React.Component{
+//     render(){
+//         return (
+//             <div>
+//                 <h1>{this.props.judul}</h1>
+//                 <h3>Harga : {this.props.harga}</h3>
+//             </div>
+//         )
+//     }
+// }
+
+const element = <Product judul = "Sempak Turbo (class component)" harga = "1M"></Product>;
+
+ReactDOM.render(element, document.getElementById('root'));
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 
